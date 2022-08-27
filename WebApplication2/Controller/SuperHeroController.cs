4d17a799c25d0f52d9ef8c73022258327cfc2950
nafic <nafic@DESKTOP-HFAJHRM>
2022-08-27 22:16:32 +0800

    Add project files.

---
+++ WebApplication2/Controller/SuperHeroController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controller
+{
+    
+    public class SuperHeroController : ApiController
+    {
+        private static List<SuperHero> supe = new List<SuperHero> {
+            new SuperHero{id=0, FirstName="Peter",LastName="Parker",HeroName="Spider-man"},
+            new SuperHero{id=1, FirstName="Tony",LastName="Stark",HeroName="Iron Man"}
+        };
+
+        [HttpGet]
+        public async Task<IHttpActionResult> Get()
+        {
+            /*var request = HttpContext.Current.Request;
+            var authHeader = request.Headers["Authorization"];
+            var authHeaderVal = AuthenticationHeaderValue.Parse(authHeader);
+            var authHeaderParam = authHeaderVal.Parameter;
+            test(authHeaderParam);
+            
+            var n = "test";*/
+            return Ok(supe);
+        }
+        public async Task<IHttpActionResult> Get(int id)
+        {
+            var hero= supe.Find(r=>r.id==id);
+            return Ok(hero);
+        }
+        public void test(string credentials)
+        {
+            var encoding = Encoding.GetEncoding("utf-8");
+            credentials = encoding.GetString(Convert.FromBase64String(credentials));
+
+            int separator = credentials.IndexOf(':');
+            string name = credentials.Substring(0, separator);
+            string password = credentials.Substring(separator + 1);
+            var test = "";
+
+        }
+
+    }
+}
---
+++ WebApplication2/Models/SuperHero.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class SuperHero
+    {
+        public int id { get; set; }
+        public string FirstName { get; set; } = String.Empty;
+        public string LastName { get; set; } = String.Empty;
+        public string HeroName { get; set; } = String.Empty;
+    }
+}
